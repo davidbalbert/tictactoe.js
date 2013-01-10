d2d687f5495db6e10d504c88aa6000c3b404a18e
David Albert <davidbalbert@gmail.com>
2013-01-10 01:21:25 -0500

    Center the board

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,3 @@
-BOARD_X = 100;
-BOARD_Y = 100;
-
 BOARD_WIDTH = 300;
 BOARD_HEIGHT = 300;
 
@@ -9,6 +6,9 @@ SQUARE_SIDE = BOARD_HEIGHT / 3;
 WIN_STATES = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
               [0, 3, 6], [1, 4, 7], [2, 5, 8],
               [0, 4, 8], [2, 4, 6]];
+
+boardX = 0;
+boardY = 0;
 
 function drawX(ctx, x, y) {
   ctx.save();
@@ -34,7 +34,7 @@ function drawO(ctx, x, y) {
 function drawBoard(ctx, board) {
   ctx.save();
   ctx.beginPath();
-  ctx.translate(BOARD_X, BOARD_Y);
+  ctx.translate(boardX, boardY);
 
   ctx.clearRect(-5, -5, BOARD_WIDTH + 10, BOARD_HEIGHT + 10);
 
@@ -53,9 +53,9 @@ function drawBoard(ctx, board) {
 
   for (i = 0; i < board.length; i++) {
     if (board[i] === 1) {
-      drawX(ctx, BOARD_X + (i % 3) * SQUARE_SIDE, BOARD_Y + Math.floor(i / 3) * SQUARE_SIDE);
+      drawX(ctx, boardX + (i % 3) * SQUARE_SIDE, boardY + Math.floor(i / 3) * SQUARE_SIDE);
     } else if (board[i] === -1) {
-      drawO(ctx, BOARD_X + (i % 3) * SQUARE_SIDE, BOARD_Y + Math.floor(i / 3) * SQUARE_SIDE);
+      drawO(ctx, boardX + (i % 3) * SQUARE_SIDE, boardY + Math.floor(i / 3) * SQUARE_SIDE);
     }
   }
 
@@ -64,7 +64,7 @@ function drawBoard(ctx, board) {
   if (winState && winState !== true) {
     ctx.save();
     ctx.beginPath();
-    ctx.translate(BOARD_X, BOARD_Y);
+    ctx.translate(boardX, boardY);
 
     if (winState[2] - winState[0] === 2) {
       var y = (0.5 + winState[0] / 3) * SQUARE_SIDE + 0.5;
@@ -88,18 +88,18 @@ function drawBoard(ctx, board) {
 }
 
 function isWithinBoard(e) {
-  return e.offsetX > BOARD_X &&
-    e.offsetX < BOARD_X + BOARD_WIDTH &&
-    e.offsetY > BOARD_Y &&
-    e.offsetY < BOARD_Y + BOARD_HEIGHT;
+  return e.offsetX > boardX &&
+    e.offsetX < boardX + BOARD_WIDTH &&
+    e.offsetY > boardY &&
+    e.offsetY < boardY + BOARD_HEIGHT;
 }
 
 function getGridIndex(e) {
   if (!isWithinBoard(e)) {
     return null;
   } else {
-    var x = e.offsetX - BOARD_X;
-    var y = e.offsetY - BOARD_Y;
+    var x = e.offsetX - boardX;
+    var y = e.offsetY - boardY;
 
     var row = Math.floor(y / SQUARE_SIDE);
     var col = Math.floor(x / SQUARE_SIDE);
@@ -128,8 +128,11 @@ function gameIsOver(board) {
 function ready() {
   var canvas = document.getElementById("c");
   var ctx = canvas.getContext("2d");
-  var board = [0,0,0,0,0,0,0,0,0];
 
+  boardX = (canvas.width - BOARD_WIDTH) / 2;
+  boardY = (canvas.height - BOARD_HEIGHT) / 2;
+
+  var board = [0,0,0,0,0,0,0,0,0];
   var currentPlayer = 1;
 
   canvas.addEventListener("mousedown", function(e) {
@@ -149,8 +152,8 @@ function ready() {
       }
     }
 
-    drawBoard(ctx, board);
+    drawBoard(ctx, board, canvas.width, canvas.height);
   });
 
-  drawBoard(ctx, board);
+  drawBoard(ctx, board, canvas.width, canvas.height);
 }
